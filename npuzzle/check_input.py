5ea0355ba3df9aad3ae78d4affe298bdff0c3af0
klin <klin@MacBook-Pro-de-klin.local>
2020-11-13 16:20:04 +0100

    first push

---
+++ npuzzle/check_input.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import sys
+
+def ft_find_rang(line):  
+	content = line.split(' ')
+	for c in content:
+		if c == "#":
+			return None
+		elif c.isdigit() and c.isnumeric():
+			return int(c)
+		elif c != " ":
+			return -1
+	return None
+
+def ft_check_line(line, rang, listnumber):
+	counter = 0
+	max = rang * rang
+	content = line.split(' ')
+	for c in content:
+		if c == '#' or c.find("#") == 0:
+			break
+		if c.isdigit() and int(c) < max and counter < max and listnumber.count(int(c)) == 0:
+			listnumber.append(int(c))
+			counter += 1
+		else:
+			return False
+	return True
+
+def ft_check_input(file_name):
+	rang = None
+	listnumber = []
+	with open(file_name, 'r') as f:
+		for line in f:
+			if rang == None:
+				rang = ft_find_rang(line)
+				if rang !=None and rang < 3:
+					print("error: ", rang)
+					return False
+			elif ft_check_line(line, rang, listnumber):
+				continue
+			else:
+				return False
+	if rang ==None or rang < 3:
+		return False
+	# print file info			
+	print(rang)
+	counter = 0
+	for ele in listnumber:
+		print(ele, " ",end="")
+		counter +=1
+		if counter == rang:
+			counter = 0
+			print("")
+	
+	return True
+
+
+			
+
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if ft_check_input(sys.argv[1]):
+			print("file ok")
+		else:
+			print("fille error")
+	else:
+		print("usage : python3 argv1 argv2")
