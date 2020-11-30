b0117938970f313171cf5e67fe3f757ded4fa88f
klin <klin@MacBook-Pro-de-klin.local>
2020-11-30 18:38:11 +0100

    entrées sorties sous forme naturelle

--- computorv1/main.py
+++ computorv1/main.py
@@ -3,85 +3,74 @@
 
 import math
 import sys
+import my_dict
 
-def ft_del_null(corrent=[]):
-	corrent.reverse()
-	while corrent and corrent[0] == 0:
-		del corrent[0]
-	corrent.reverse()
-	return corrent
-
-def ft_filter(s):
-	ele = []
-	start = 0
-	add = s.find("+")
-	while add != -1:
-		ele.append(s[start:add])
-		start = add
-		add = s.find("+", add+1)
-	ele.append(s[start:])
-
-	final = []
-	for e in ele:
-		start = 0
-		sub = e.find("-")
-		while sub != -1:
-			final.append(e[start:sub])
-			start = sub
-			sub = e.find("-", sub+1)
-		final.append(e[start:])
-
-	info = []
-	for e in final:
-		split = e.split("*X^")
-		try:
-			info.append(int(split[0]))
-		except Exception:
-			info.append(float(split[0]))
-	return ft_del_null(info)
-
-def ft_reduce(e1,e2):
-	my_equation = []
-	for i in range(len(e1)):
-		if i < len(e2):
-			my_equation.append(e1[i] - e2[i])
-		else:
-			my_equation.append(e1[i])
-	return ft_del_null(my_equation)
+def ft_format(d, i):
+	coef = ""
+	degre = ""
+	if i == 1:
+		degre = "X"
+	elif i > 1:
+		degre = "X^" + str(i)
+	
+	if d[i] == 1 and degre < 1:
+		coef = str(abs(d[i]))
+	else:
+		coef = str(abs(d[i]))
+	return coef + degre
 
 def ft_check_equation(s):
+	#split str to 2 equations and del empty
 	del_space = s.split()
 	s = "".join(del_space)
 	my_list = s.split("=")
-	f1 = ft_filter(my_list[0])
-	f2 = ft_filter(my_list[1])
+	e1 = my_dict.ft_filter(my_list[0])
+	e2 = my_dict.ft_filter(my_list[1])
 
-	my_equation = []
-	if f1 == f2:
+	# reduce equation and del empty
+	my_equation = {}
+	if e1 == e2:
 		print("All real numbers are solution.")
 		return []
-	if len(f1) >= len(f2):
-		my_equation = ft_reduce(f1, f2)
+	if  e1["max"] >= e2["max"]:
+		my_equation["max"] = e1["max"] 
+		my_equation = my_dict.ft_reduce(e1, e2, e1["max"])
 	else:
-		my_equation = ft_reduce(f2, f1)
-	
-	for i in range(len(my_equation)):
+		my_equation["max"] = e2["max"] 
+		my_equation = my_dict.ft_reduce(e2, e1, e2["max"])
+
+	# print reduced equation and Polynomial degree
+	i = 0
+	max = my_equation["max"]
+	bool_first = True
+	while i <= max:
 		if i == 0:
-			print(f"Reduced form: {my_equation[i]} * X^{i} ", end="")
-		elif my_equation[i] >= 0:
-			print(f"+ {my_equation[i]} * X^{i} ", end="")
-		else:
-			print(f"- {abs(my_equation[i])} * X^{i} ", end="")
+			print(f"Reduced form: ", end="")
+		elif i in my_equation.keys() and my_equation[i] > 0:
+			if bool_first:
+				print(f"{ft_format(my_equation, i)} ", end="")
+				bool_first = False
+			else:
+				print(f"+ {ft_format(my_equation, i)} ", end="")
+		elif i in my_equation.keys() and my_equation[i] < 0:
+			print(f"- {ft_format(my_equation, i)} ", end="")
+		i += 1
 	print("= 0")
-	print(f"Polynomial degree: {len(my_equation) - 1}")
+	print(f"Polynomial degree: {max}")
 	return my_equation
 
-def solve(c, b, a):
+def solve(equation):
+	a = my_dict.ft_dict_value(equation, 2)
+	b = my_dict.ft_dict_value(equation, 1)
+	c = my_dict.ft_dict_value(equation, 0)
+
+	# degree 1
 	if a == 0:
 		print("The solution is:")
 		print((-c) / b)
 		return
 
+	# degree 2
 	delta = b**2 - (4 * a * c)
 	if delta >= 0:
 		i = math.sqrt(delta)
@@ -102,15 +91,15 @@ def solve(c, b, a):
 
 if __name__ == "__main__":
 	if len(sys.argv) == 2:
-		l = []
 		equation = ft_check_equation(sys.argv[1])
-		if len(equation) > 3:
-			print("The polynomial degree is strictly greater than 2, I can't solve.")
-		elif len(equation) == 3:
-			solve(equation[0], equation[1], equation[2])
-		elif len(equation) == 2:
-			solve(equation[0], equation[1], 0)
-		elif len(equation) == 1:
-			print("Warning: the polynomial is unsolvable!")
+		if equation:
+			if equation["max"] >= 3:
+				print("The polynomial degree is strictly greater than 2, I can't solve.")
+			elif equation["max"] >= 1:
+				solve(equation)
+			elif equation["max"] <= 0:
+				print("Warning: the polynomial is unsolvable!")
+	elif len(sys.argv) == 3 and sys.argv[1] == "-b":
+		print("bonus")
 	else:
-		print("usage: python3 main.py equation")
+		print("usage: python3 main.py [-b] equation")
---
+++ computorv1/my_dict.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+# return dict[key] value
+# return 0 if not key in dict
+def ft_dict_value(d, k):
+	if k in  d.keys():
+		return d[k]
+	else:
+		return 0
+
+# del dict empty value, set new max(degree)
+def ft_del_empty(corrent={}):
+	i = corrent["max"]
+	max = 0
+	while i >= 0:
+		if i in corrent.keys():
+			if corrent[i] == 0:
+				del corrent[i]
+			else:
+				if i > max :
+					max = i
+		i -= 1
+	corrent["max"] = max
+	return corrent
+
+# add dict[degre, coef]
+def ft_append(coef, degre, d):
+	try:
+		d[degre] = (int(coef))
+	except Exception:
+		d[degre] = (float(coef))
+
+def ft_find_coef(e):
+	if e.find("X") >= 0:
+		split = e.split("X")
+		if split[0].find("*") >= 0:
+			return split[0][0:split[0].find("*")]
+		elif len(split[0]) == 1:
+			if split[0].find("+") == 0 or split[0].find("-") == 0:
+				return split[0] + "1"
+			else:
+				return split[0]
+		elif len(split[0]) == 0:
+			return "1"
+		else:
+			return split[0]
+	else:
+		return e
+
+def ft_find_degre(e):
+	if e.find("X") >= 0:
+		split = e.split("X")
+		if split[1].find("^") >= 0:
+			return split[1][split[0].find("^"): ]
+		else:
+			return "1"
+	else:
+		return "0"
+
+# str split by "+" and "-" => list [a ∗ x^p]
+# with ft_find_coef and ft_find_degre => get coef and degree
+# result: dict[p] = a, a: coef p: degre
+def ft_filter(s):
+	ele = []
+	start = 0
+	add = s.find("+")
+	while add != -1:
+		ele.append(s[start:add])
+		start = add
+		add = s.find("+", add+1)
+	ele.append(s[start:])
+
+	final = []
+	for e in ele:
+		start = 0
+		sub = e.find("-")
+		while sub != -1:
+			final.append(e[start:sub])
+			start = sub
+			sub = e.find("-", sub+1)
+		final.append(e[start:])
+
+	info = {}
+	info["max"] = 0
+	for e in final:
+		if e:
+			coef = ft_find_coef(e)
+			degre = int(ft_find_degre(e))
+			if degre > info["max"]:
+				info["max"] = degre
+			ft_append(coef, degre, info)
+	return ft_del_empty(info)
+
+# e = e1 - e2, and del empty
+def ft_reduce(e1, e2, max):
+	my_equation = {}
+	my_equation["max"] = max
+	i = max
+	while i >= 0:
+		if i in e1.keys() and i in e2.keys():
+			my_equation[i] = e1[i] - e2[i]
+		elif i in e1.keys() and not(i in e2.keys()):
+			my_equation[i] = e1[i]
+		elif not(i in e1.keys()) and i in e2.keys():
+			my_equation[i] = 0 - e2[i]
+		i -= 1
+	return ft_del_empty(my_equation)
